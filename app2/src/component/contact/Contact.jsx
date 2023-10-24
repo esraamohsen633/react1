130b9462e7275cc651267020583c2831358ad1b4
Esraamohsen633 <esraamohsen@gmail.com>
2023-10-24 21:35:10 +0300

    app2

--- app2/src/component/contact/Contact.jsx
+++ app2/src/component/contact/Contact.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+
+import axios from 'axios';
+import React, { useEffect , Component } from 'react'
+
+
 
 export default function Contact() {
+
+  async function getData(){
+    let {data} =await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+console.log(data);
+
+  }
+  useEffect(()=>{
+    getData()
+
+  },[])
   return (
     <div>Contact</div>
   )
 }
+
+
+
